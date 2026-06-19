71d3b975af9417f2fd80a40bfad723b44a5ed110
ronia <ronia@m57>
2026-06-19 12:31:43 +0300

    style: fix ktlint import ordering (LaunchedEffect before collectAsState)

--- app/src/main/java/com/m57/hermescontrol/Navigation.kt
+++ app/src/main/java/com/m57/hermescontrol/Navigation.kt
@@ -46,8 +46,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope

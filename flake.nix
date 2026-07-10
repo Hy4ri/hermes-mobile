978ecfcae052f8786ec5aaffa0f0b096a0a50ad6
Hy4ri <hiari45@gmail.com>
2026-07-10 22:24:05 +0300

    nix: flake

--- flake.nix
+++ flake.nix
@@ -21,39 +21,40 @@
           };
         };
 
-        buildToolsVersion = "35.0.0";
-        androidSdk = (pkgs.androidenv.composeAndroidPackages {
-          # Command-line & platform tools
-          cmdLineToolsVersion = "11.0";
-          platformToolsVersion = "35.0.2";
+        buildToolsVersion = "36.0.0";
+        androidSdk =
+          (pkgs.androidenv.composeAndroidPackages {
+            # Command-line & platform tools
+            cmdLineToolsVersion = "11.0";
+            platformToolsVersion = "35.0.2";
 
-          # Build tools
-          buildToolsVersions = [buildToolsVersion "34.0.0"];
+            # Build tools
+            buildToolsVersions = [buildToolsVersion];
 
-          # Target platforms (36 required by AGP 9.0.1 / compileSdk 36)
-          platformVersions = ["36" "35" "34"];
+            # Target platforms (36 required by AGP 9.0.1 / compileSdk 36)
+            platformVersions = ["36"];
 
-          # Emulator + system images for local AVD testing
-          includeEmulator = true;
-          includeSystemImages = true;
-          systemImageTypes = ["google_apis"];
-          abiVersions = ["x86_64"];
+            # Emulator + system images for local AVD testing
+            includeEmulator = true;
+            includeSystemImages = true;
+            systemImageTypes = ["google_apis"];
+            abiVersions = ["x86_64"];
 
-          # NDK (not needed for this project, but handy)
-          includeNDK = false;
+            # NDK (not needed for this project, but handy)
+            includeNDK = false;
 
-          # Extra packages
-          extraLicenses = [
-            "android-googletv-license"
-            "android-sdk-arm-dbt-license"
-            "android-sdk-license"
-            "android-sdk-preview-license"
-            "google-gdk-license"
-            "intel-android-extra-license"
-            "intel-android-sysimage-license"
-            "mips-android-sysimage-license"
-          ];
-        }).androidsdk;
+            # Extra packages
+            extraLicenses = [
+              "android-googletv-license"
+              "android-sdk-arm-dbt-license"
+              "android-sdk-license"
+              "android-sdk-preview-license"
+              "google-gdk-license"
+              "intel-android-extra-license"
+              "intel-android-sysimage-license"
+              "mips-android-sysimage-license"
+            ];
+          }).androidsdk;
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
